27dada57ec992c7a847d14d57f0bddbfb74d8db4
Mohakim12 <mohammadriswan12@gmail.com>
2023-04-06 22:48:08 +0700

    webphp

--- application/views/biodata.php
+++ application/views/biodata.php
@@ -1,3 +1,10 @@
+</html>
+</body>
+
+    
 Nama  : Mohammad Riswan Nurhakim
 NIM   : 17200427
 KELAS : 17.6A.26
+
+</body>
+</html>
---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Mohammad Riswan</li>
+ <li>Nama Belakang : Nurhakim</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan G1 No. 40A Slipi</li>
+ </ul>
+
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Jakarta</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Sepakbola</li>
+ <li>Renang</li>
+ </ul>
+</section>
